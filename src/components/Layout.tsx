f07474062e556bf991326ecd30a0016633ae2893
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2022-10-12 13:24:19 -0400

    Cut v0.0.2 release
    Signed-off-by: Andriy Kalashnykov <AndriyKalashnykov@gmail.com>

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -22,7 +22,7 @@ import Logo from './Logo'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 
-const Version = "v0.0.1"
+const Version = "v0.0.2"
 
 const LanguageSwitch = () => {
   const [anchorEl, setAnchorEl] = useState(null)
